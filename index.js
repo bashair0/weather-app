2b6b0ca6dadea6118afd6418d0e3ac2f4cbeb04b
bashair0 <bisho5.ba@gmail.com>
2024-08-25 14:24:46 +0800

    Get weather data from the api  and display it

---
+++ index.js
@@ -0,0 +1,39 @@
+document.addEventListener('DOMContentLoaded', () => {
+  document.querySelector('form').onsubmit = e => {
+    e.preventDefault()
+    const city = document.querySelector('#city').value
+    document.querySelector('#cityName').innerHTML = city
+
+    getWeatherData(city)
+  }
+
+  async function getWeatherData (city) {
+    try {
+      const response = await fetch(
+        `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?key=H9NYJX84SDMP95FQ5SL9TL2T6 `,
+        { mode: 'cors' }
+      )
+      const weatherData = await response.json()
+
+      const conditions = weatherData.currentConditions.conditions
+      const icon = weatherData.currentConditions.icon
+      const fahrenheit = weatherData.currentConditions.temp
+      const uvIndex = weatherData.currentConditions.uvindex
+      const time = weatherData.currentConditions.datetime
+
+      const celsius = fah => {
+        return parseInt(((fah - 32) * 5) / 9)
+      }
+
+      document.querySelector('#img').src = updateImg(icon)
+      document.querySelector('#conditions').innerHTML = conditions
+      document.querySelector('#fahrenheit').innerHTML = fahrenheit
+      document.querySelector('#celsius').innerHTML = celsius(fahrenheit)
+      document.querySelector('#uvindex').innerHTML = uvIndex
+      document.querySelector('#time').innerHTML = time
+    } catch (err) {
+      document.querySelector('#cityName').innerHTML =
+        'No matching location found'
+    }
+  }
+})
